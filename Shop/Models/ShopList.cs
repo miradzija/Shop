a9d866da789792b3dbbf9f8ce809dc0d9730be43
Zile <zivanovicmilos86@gmail.com>
2022-12-24 00:56:57 +0100

    DbContext

---
+++ Shop/Models/ShopList.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shop.Models
+{
+    public class ShopList
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string ProductName { get; set; }
+        public string Size { get; set; }
+        public double Price { get; set; }
+    }
+}
