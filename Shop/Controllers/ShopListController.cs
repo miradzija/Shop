be993991521f037747ae78def3d9d8f825eded60
Zile <zivanovicmilos86@gmail.com>
2022-12-24 19:19:57 +0100

    validation and CRUD

--- Shop/Controllers/ShopListController.cs
+++ Shop/Controllers/ShopListController.cs
@@ -19,5 +19,66 @@ namespace Shop.Controllers
         }
 
 
+        //GET
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(ShopList obj)
+        {
+            if (obj.ProductName == obj.Size.ToString())
+            {
+                ModelState.AddModelError("name", "You fucked up");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.shopLists.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        //GET
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var shopListFromDb = _db.shopLists.Find(id);
+
+            if (shopListFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(shopListFromDb);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(ShopList obj)
+        {
+            if (obj.ProductName == obj.Size.ToString())
+            {
+                ModelState.AddModelError("name", "You fucked up");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.shopLists.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+
     }
 }
--- Shop/Models/ShopList.cs
+++ Shop/Models/ShopList.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Shop.Models
 {
@@ -7,8 +8,10 @@ namespace Shop.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        [DisplayName("Dreska")]
         public string ProductName { get; set; }
         public string Size { get; set; }
+        [Range(1, 20000, ErrorMessage = "Mnogo to pare bate!!!")]
         public double Price { get; set; }
     }
 }
